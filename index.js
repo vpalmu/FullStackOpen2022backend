66065e45ae63de86f1570cdc3154b58194e5e2a7
palmuves <vesa.palmu@tietoevry.com>
2023-01-30 18:05:42 +0200

    added fronent build folder as a static content in backend project

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(express.json())             // activate the json-parser for POST request
 const cors = require('cors')        // cors middleware
 app.use(cors())
 
+// Whenever express gets an HTTP GET request it will first check if the build directory contains a file corresponding 
+// to the request's address. If a correct file is found, express will return it.
+// Now HTTP GET requests to the address www.serversaddress.com/index.html or www.serversaddress.com will 
+// show the React frontend. GET requests to the address www.serversaddress.com/api/notes will be handled by the backend's code.
+app.use(express.static('build'))    // load 'build' folder as a static content (frontend app production version compiled)
+
 // Middleware functions have to be taken into use before routes if we want them to be executed before the route event handlers are called
 morgan.token('request-log', function(req, res) {
     if (req.method === 'POST' && req.url === '/api/persons') {
