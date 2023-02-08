c2d08505fe25e735869d4d0dc474adaa51981e7f
palmuves <vesa.palmu@tietoevry.com>
2023-02-08 13:39:46 +0200

    added lint rule to ignore unused arguments

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,9 @@ module.exports = {
         'arrow-spacing': [
             'error', { 'before': true, 'after': true }
         ],
-        'no-console': 0
+        'no-console': 0,
+        'no-unused-vars': [
+            'error', { 'args': 'none' }
+        ]
     }
 }
