c8a4e5032f511634a56db14d73f591894e7f36f7
palmuves <vesa.palmu@tietoevry.com>
2023-02-09 10:26:18 +0200

    fixed comments

--- utils/config.js
+++ utils/config.js
@@ -2,22 +2,15 @@ const logger = require('../utils/logger')
 
 // use PORT from .env file only when running locally. In production, Fly.io defines the PORT
 // dynamically via it's own environment variable
+//
 if (process.env.NODE_ENV !== 'production') {
     logger.info('development environment..')
+    // With dotenv usage, the environment variables defined in the .env file override the values in defined in environment (OS)
     require('dotenv').config()
     logger.info('process.env.PORT:', process.env.PORT)
     logger.info('process.env.MONGODB_URI:', process.env.MONGODB_URI)
 }
 
-//--------------------------------------------------
-// - try to get value for PORT
-//   - if api is not running in production, process.env.port tries to read it from enviroment variable (OS)
-//      - if environment variable (OS) is not defined, try to read it from .env file
-//   - if app is running in production, Fly.io will try to read it only from environment variable (because dotenv is not in use)
-//   - in either case, if PORT does not have the value defined, default to 3001
-//
-//     PATH in environment (OS) -> PATH in .env file -> default 3001
-//
 const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI
 
