6436a9de87a56fa8a4ed9f39ee7aecdb1a36c19b
palmuves <vesa.palmu@tietoevry.com>
2023-02-13 09:57:33 +0200

    installed and configured cross-env package

--- utils/config.js
+++ utils/config.js
@@ -12,7 +12,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 module.exports = {
     MONGODB_URI,
